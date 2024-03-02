ef4793e257772579d1694994da2b79307899d90e
Er2c123 <58917343+Er2c123@users.noreply.github.com>
2024-03-02 11:39:43 -0500

    Create Item.java

---
+++ Item.java
@@ -0,0 +1,29 @@
+package test;
+
+public class Item {
+    String name;
+    double price;
+    int quantity;
+
+    public Item(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
